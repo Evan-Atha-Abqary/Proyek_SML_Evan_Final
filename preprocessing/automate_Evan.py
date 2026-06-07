28793f9deee4e17fd6b7fd47683fffbc21d821ab
Evan Atha Abqary <evan.abqary05@gmail.com>
2026-06-08 05:57:22 +0700

    Membuat ulang struktur folder Eksperimen SML dari nol

--- preprocessing/automate_Evan.py
+++ preprocessing/automate_Evan.py
@@ -4,19 +4,20 @@ import os
 def run_preprocessing():
     print("Mulai membaca data mentah...")
     
-    # --- PERBAIKAN JALUR (PATH) DI SINI ---
-    # 1. Mengambil lokasi folder persis tempat script automate_Evan.py ini berada
+    # 1. Mengambil lokasi folder persis tempat script ini berada
     current_dir = os.path.dirname(os.path.abspath(__file__))
     
-    # 2. Menggabungkan jalur secara absolut ke dataset_raw
+    # 2. Menggabungkan jalur secara absolut ke folder dataset_raw
     raw_path = os.path.join(current_dir, '..', 'dataset_raw', 'credit_risk_dataset.csv')
-    # --------------------------------------
     
     df = pd.read_csv(raw_path)
     
     print("Mulai membersihkan data...")
+    # Mengatasi data kosong
     df['person_emp_length'] = df['person_emp_length'].fillna(df['person_emp_length'].median())
     df['loan_int_rate'] = df['loan_int_rate'].fillna(df['loan_int_rate'].median())
+    
+    # Menghapus duplikat & Encoding
     df.drop_duplicates(inplace=True)
     df_clean = pd.get_dummies(df, drop_first=True)
     
@@ -25,7 +26,7 @@ def run_preprocessing():
     clean_path = os.path.join(current_dir, 'credit_risk_dataset_clean.csv')
     df_clean.to_csv(clean_path, index=False)
     
-    print(f"Selesai! Data bersih berhasil diperbarui dan disimpan di: {clean_path}")
+    print(f"Selesai! Data bersih berhasil diperbarui dan disimpan di:\n{clean_path}")
     return df_clean
 
 if __name__ == "__main__":
